ad86d2d7b917d8fa050d761e16f388fd27d92004
n00bcybot <freshmaker21@gmail.com>
2025-03-31 22:21:09 +0200

    Correcting project folder structure

--- Main/UI/main_window.py
+++ Main/UI/main_window.py
@@ -8,17 +8,12 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
-    QCursor, QFont, QFontDatabase, QGradient,
-    QIcon, QImage, QKeySequence, QLinearGradient,
-    QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
-    QSizePolicy, QStatusBar, QWidget)
-import Icons_rc
+from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect,
+                            QSize)
+from PySide6.QtGui import (QAction, QFont, QIcon)
+from PySide6.QtWidgets import (QMenu, QMenuBar,
+                               QStatusBar, QWidget)
+
 
 class Ui_mw_Main(object):
     def setupUi(self, mw_Main):
